f8f92ac059cfcfcf8d67f722b324a415e09c704b
Tyris <tyristhompson19@gmai.com>
2025-08-13 11:44:20 -0500

    Added URL Validation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const businessInfo = document.querySelector('.business-info');
 const accountAccess = document.querySelector('.account-access');
+const form = document.querySelector('form');
+const input = document.querySelector('#url-input');
+const inputContainer = document.querySelector('.enter-url');
+const errorMessage = document.createElement('p');
+errorMessage.className = 'error-message';
 const mediaQuery = window.matchMedia('(max-width: 650px)');
 
 function setNavStyles(mq) {
@@ -25,6 +30,7 @@ function setNavStyles(mq) {
     <p>Resources</p>`;
   }
 }
+
 
 function openMenu() {
   accountAccess.classList.toggle('open')
@@ -55,3 +61,68 @@ function openMenu() {
 
 setNavStyles(mediaQuery);
 mediaQuery.addEventListener('change', setNavStyles);
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const url = input.value.trim();
+  if(url === "") {
+    errorMessage.textContent = `Please enter a url.`;
+    inputContainer.appendChild(errorMessage);
+    input.classList.add('error');
+    input.addEventListener('keypress', removeError);
+    return
+  }
+  isValidURL(url);
+};
+
+
+function isValidURL(url) {
+  try {
+    const validURL = new URL(url);
+    const encodedURL = encodeURIComponent(validURL);
+    shortenURL(encodedURL);
+
+  } catch (error) {
+    errorMessage.textContent = `The URL you entered is not valid.`;
+    inputContainer.appendChild(errorMessage);
+    input.classList.add('error');
+    input.addEventListener('keydown', removeError);
+    console.log(error);
+  }
+}
+
+
+function removeError() {
+  if(input.classList.contains('error')) {
+    input.classList.remove('error');
+    inputContainer.removeChild(errorMessage);
+  }
+}
+
+
+async function shortenURL(validURL) {
+  try {
+    const response = await fetch(
+    "https://corsproxy.io/?" + "https://cleanuri.com/api/v1/shorten",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        url: validURL,
+      }),
+    }
+  );
+
+  const data = await response.json();
+  console.log(data.result_url);
+} catch (error) {
+      console.log(error);
+  }
+}
+
+
+
+
